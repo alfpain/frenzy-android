cc128edfe3f602253b7990c562d6035d4623145a
alfpain <kndalf@gmail.com>
2015-09-26 22:42:09 -0600

    login facebook cordova

---
+++ platforms/android/com.phonegap.plugins.facebookconnect/armar580136-FacebookLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/brandon/Música/frenzy/platforms/android/com.phonegap.plugins.facebookconnect/armar580136-FacebookLib/bin/AndroidManifest.xml : \
+/home/brandon/Música/frenzy/platforms/android/com.phonegap.plugins.facebookconnect/armar580136-FacebookLib/AndroidManifest.xml \
